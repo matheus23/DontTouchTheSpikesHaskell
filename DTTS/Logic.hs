d90cf5c93404103ec7dba5d7f44ee91c0a862b76
matheus23 <matheus.dev@googlemail.com>
2015-02-06 17:56:48 +0100

    Initially have no Spikes on the first wall.

--- DTTS/Logic.hs
+++ DTTS/Logic.hs
@@ -44,8 +44,7 @@ screensBehaviour = fold (PreGame $ initialGS $ mkStdGen 42) simulateScreen
 
 
 initialGS :: StdGen -> GameState
-initialGS rand = GameState (gameWidth/2, gameHeight/2) (playerMoveVel 0, playerJumpVel) spikes [] 0 rand' 0
-  where (spikes, rand') = genSpikes rand 0
+initialGS rand = GameState (gameWidth/2, gameHeight/2) (playerMoveVel 0, playerJumpVel) [] [] 0 0
 
 genSpikes :: StdGen -> Int -> ([Int], StdGen)
 genSpikes rand score = (spikes, newRand)
